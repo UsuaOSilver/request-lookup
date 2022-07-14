20a0603535651c9ec9ce725abc575b1de3d5eb2f
napee19 <napee1904@gmail.com>
2022-07-14 14:25:02 -0700

    set up rpc provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import './App.css';
 import { ethers } from 'ethers';
 
 function App() {
+  
+  const provider = new InfuraProvider("homestead", {
+    projectID: REACT_APP_PROJECT_ID,
+    projectSecret: REACT_APP_PROJECT_SECRET 
+  });
+  
+  const wbtc = {
+    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    abi: [
+        "function mint(addres _to, uint256 _amount) public returns (bool)",
+        "function burn(uint256 _amount) public"
+    ],
+  };
+  
   return (
     
   );
