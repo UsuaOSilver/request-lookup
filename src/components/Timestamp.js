dd02ffa5bbc1b8f229904732cab328c386fa8ffb
napee19 <napee1904@gmail.com>
2022-07-19 22:20:09 -0700

    convert time from unix to date time format

--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,23 +1,26 @@
 import { ethers } from 'ethers';
+import { fromUnixTime, format } from 'date-fns'
 import { useEffect, useState } from 'react';
 
 export default function Timestamp({blockNumber}) {
     
-    const [timestamp, setTimestamp] = useState(null);
+    const [time, setTime] = useState(null);
     
     useEffect(() => {
         const provider = new ethers.providers.WebSocketProvider("wss://eth-mainnet.g.alchemy.com/v2/sJzaXe5o3w8nxLI19QtoP-qXT2hlvKZQ", 1);
         
-        const getTimestamp = async () => {
+        const getTime = async () => {
             const block = await provider.getBlock(blockNumber)
             const timestamp = block.timestamp
-            setTimestamp(timestamp)
+            const time = format(fromUnixTime(timestamp), 'MM/dd/yyyy ppp')
+            console.log(time)
+            setTime(time)
         }
-        getTimestamp()
+        getTime()
     // eslint-disable-next-line 
     }, []);
     
     return (
-        <td className="ml12">{timestamp}</td>
+        <td className="ml12">{time}</td>
     )
 }
